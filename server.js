38583b31e9116509fd85bc29bfbc767b230c5392
adsul chiru17 <adsulchiranjiv958@gmail.com>
2024-05-20 03:22:21 +0530

    mern

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ dotenv.config();
 const app = express();
 
 const corsConfig = {
-  origin: "http://localhost:5173",
+  origin: "  https://lively-brigadeiros-a4ddd9.netlify.app/",
+  // http://localhost:5173
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
