b1f2eaa2f2dcfe0a216e888479051dda0b1076a3
adsul chiru17 <adsulchiranjiv958@gmail.com>
2024-05-20 03:37:35 +0530

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 const corsConfig = {
-  origin: "https://thunderous-pavlova-31149b.netlify.app/",
+  origin: "https://thunderous-pavlova-31149b.netlify.app",
   // http://localhost:5173
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
