ca73b0ce49307e15892ab68c93f6c60792592c1c
adsul chiru17 <adsulchiranjiv958@gmail.com>
2024-05-20 15:32:03 +0530

    update

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const app = express();
 
 const corsConfig = {
   origin: "https://thunderous-pavlova-31149b.netlify.app",
-  // http://localhost:5173
+  // origin: "http://localhost:5173",
+
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
